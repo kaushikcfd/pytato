2be2c884fc0606da831a537dde3402b0f06f0c33
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-11-01 00:27:31 -0500

    define pt.reshape

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -417,6 +417,37 @@ def test_dict_of_named_array_codegen_avoids_recomputation():
     assert ("y" in knl.id_to_insn["z_store"].read_dependency_names())
 
 
+@pytest.mark.parametrize("oldshape,newshape", itertools.product(
+                                      [(36,),
+                                       (3, 3, 4),
+                                       (12, 3),
+                                       (2, 2, 3, 3, 1)],
+                                      [(-1,),
+                                       (-1, 6),
+                                       (4, 9),
+                                       (9, -1),
+                                       (36, -1)]))
+def test_reshape(ctx_factory, oldshape, newshape):
+    cl_ctx = ctx_factory()
+    queue = cl.CommandQueue(cl_ctx)
+
+    from numpy.random import default_rng
+    rng = default_rng()
+    x_in = rng.random(size=oldshape)
+
+    namespace = pt.Namespace()
+    x = pt.make_data_wrapper(namespace, x_in)
+    expected_out = np.reshape(x_in, newshape=newshape)
+
+    prog = pt.generate_loopy(
+            pt.reshape(x, newshape=newshape),
+            target=pt.PyOpenCLTarget(queue))
+
+    _, (out,) = prog()
+    assert expected_out.shape == out.shape
+    assert (out == expected_out).all()
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         exec(sys.argv[1])
